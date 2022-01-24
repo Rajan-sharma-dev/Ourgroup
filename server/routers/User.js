1cedb49fc2fa32af080f22e0111bfb9dc195b279
Rajan <rajan121.vishal121@gmail.com>
2022-01-24 10:58:40 +0530

    saving app.js

--- server/routers/User.js
+++ server/routers/User.js
@@ -5,41 +5,30 @@ const bodyParser = require('body-parser');
 const app=express()
 app.use(bodyParser());
 const router=express.Router()
-<<<<<<< HEAD
-const User=require('../model/user')
-=======
 // console.log("create")
-
 const User=require('../model/user')
-
->>>>>>> 9321edee979293d89141c18858a07522a24b40da
 router.post('/registers',async(req,res)=>{
-   
+    // console.log(req.body)
     const { name,email,phone,password,cpassword,pincode,state,district,address } =req.body
         if(!name || !email || !phone || !password || !cpassword || !pincode || !state || !district || !address){
              res.json({status:"failed",message:"invalid"})
         }
     try{
         const userExist =await User.findOne({email:email})
-      
+        // console.log(userExist)
         if(userExist){
              res.json({message:"User Exist"})
         }
-
-        
+        // console.log(name,email,phone,password,cpassword,pincode,state,district,address)
         const hash= await bcrypt.hash(password,10)
         const chash=await bcrypt.hash(cpassword,10)
-      
-   
+        // console.log(hash)
         const user =  new User({name,email,phone,password:hash,cpassword:chash,state,district,address,pincode})
-     
-        
+        // console.log(user,'uuu')
         const userRegister=  user.save()
-       
-
+        // console.log(userRegister)
         if(userRegister){
             res.json({status:'success',message:"sign up success",user:userRegister})
-
         }
         else{
             res.json({status:"failed",message:"invalid credentials"})
@@ -47,39 +36,27 @@ router.post('/registers',async(req,res)=>{
     }
     catch(err){
         res.json({status:"failed",message:"err.message"})
-      
     }
 })
 router.post("/login",async(req,res)=>{
     try{
         let token
-        const { email,password } = req.body
-        console.log(email,password)
+        const { email ,password } = req.body
         if(!email || !password){
             res.json({message:"empty"})
         }
         const userLogin=await User.findOne({email:email})
-        console.log(userLogin)
-        let isCheck
         if(userLogin){
-            isCheck= await bcrypt.compare(password,userLogin.password)
-           
-
-        if(!isCheck){
-            res.json({message:"Invalid password"})
-        }else{
+            const isCheck= await bcrypt.compare(password,userLogin.password)
             token=await userLogin.generateAuthToken()
             console.log(token)
             res.cookie("jwttoken",token,{
                 expires:new Date(Date.now()+ 25892000000),
                 httpOnly:true
             })
-<<<<<<< HEAD
-=======
         if(!isCheck){
             res.json({message:"Invalid password"})
         }else{
->>>>>>> 9321edee979293d89141c18858a07522a24b40da
             res.json("Login Successful")
         }
         }else{
@@ -89,5 +66,4 @@ router.post("/login",async(req,res)=>{
         console.log(err)
     }
 })
-
 module.exports = router;
